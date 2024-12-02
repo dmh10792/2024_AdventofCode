a21b5a19495ae4fb43c236fc4fabe8cecfb00e16
Herring, Desmond M CW2 USARMY 10 MTN DIV 1 BCT (USA) <desmond.m.herring.mil@army.mil>
2024-12-02 10:25:54 -0600

    finished day 1 part 1

---
+++ Day1/src/Main.java
@@ -0,0 +1,64 @@
+import java.io.File;
+import java.util.*;
+
+import static java.lang.Math.abs;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Integer> leftList = new ArrayList<>();
+        List<Integer> rightList = new ArrayList<>();
+
+        int total = 0;//initialize numbers to return
+
+        /*
+         * Read in the lists
+         */
+        try {
+            File inputFile = new File("src/input.txt");
+            Scanner myReader = new Scanner(inputFile);
+
+            while (myReader.hasNextLine()) {
+
+                String line = myReader.nextLine();//read in the next line
+
+                String[] numbers = line.split("\\s+");//split into string array
+                leftList.add(Integer.parseInt(numbers[0]));//add first one to left list and convert to int
+                rightList.add(Integer.parseInt(numbers[1]));//add second one to right list and convert to int
+
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        /*
+         * Sort the lists
+         */
+        Collections.sort(leftList);
+        Collections.sort(rightList);
+
+        /*
+            compare the lists for the difference between each value and add them to a total
+         */
+        for (int i = 0; i < leftList.size(); i++) {
+            int num1 = leftList.get(i);
+            int num2 = rightList.get(i);
+
+            int diff = Math.abs(num1 - num2);
+
+            total += diff;
+        }
+
+        //return/print the total
+        System.out.println(total);
+    }
+
+}
+
+
+
+
+//return/print the total
+
