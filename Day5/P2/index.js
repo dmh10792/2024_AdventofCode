e32de4fd5cfbfcba1e57c959c0a8aec13c55fd1f
Herring, Desmond M CW2 USARMY 10 MTN DIV 1 BCT (USA) <desmond.m.herring.mil@army.mil>
2024-12-05 20:50:03 -0600

    Done with part 2

--- Day5/P2/index.js
+++ Day5/P2/index.js
@@ -8,7 +8,7 @@ let result = 0;
 
 //Where things actually happen
 try {
-    let input = fs.readFileSync('../P1/TestInput.txt', 'utf8')
+    let input = fs.readFileSync('../P1/input.txt', 'utf8')
         .replace(/\r\n/g, '\n') // Normalize Windows line endings
         .replace(/\r/g, '\n')
         .split(/\n\n/); 
@@ -16,6 +16,10 @@ try {
     rules = input[0].split(/\n/);
     
     pages = input[1].split(/\n/);
+
+    for(let i = 0; i < pages.length; i++) {
+        pages[i] = pages[i].split(',');
+    }
     
 } catch (error) {
     console.log(error)
@@ -25,38 +29,53 @@ const checkPages = () => {
     //for each page
     pages.forEach(pageList => {
         let safe = true;
+        let firstSafe = true;
+
         //for each rule
         for(let i = 0; i < rules.length; i++) {
             let rule = rules[i].split(/\|/);
-            //check if both pages are in the pages list
-            if(pageList.includes(rule[0]) && pageList.includes(rule[1])) {
-                //if it is make sure the rule is followed
-                let firstPageIndex = pageList.indexOf(rule[0]);
-                let secondPageIndex = pageList.indexOf(rule[1]);
+                //check if both pages are in the pages list
+                if(pageList.includes(rule[0]) && pageList.includes(rule[1])) {
+                    //if it is make sure the rule is followed
+                    let firstPageIndex = pageList.indexOf(rule[0]);
+                    let secondPageIndex = pageList.indexOf(rule[1]);
 
-                if(firstPageIndex > secondPageIndex) {//if its not safe
-                    //swap the two numbers
-                    let num1 = pageList[firstPageIndex];
-                    let num2 = pageList[secondPageIndex];
-                    let temp = num1;
-                    pageList[firstPageIndex] = num2;
-                    pageList[secondPageIndex] = num1;
-                    safe = false;
+                    if(firstPageIndex > secondPageIndex) {//if its not safe
+                        //swap the two numbers
+                        [pageList[firstPageIndex], pageList[secondPageIndex]] = 
+                            [pageList[secondPageIndex], pageList[firstPageIndex]];
+                        safe = false;
+                        firstSafe = false;
+                    }
                 }
-            }
-                
+        }
 
+        while(!safe) {
+            safe = true;
+            for(let i = 0; i < rules.length; i++) {
+                let rule = rules[i].split(/\|/);
+                    //check if both pages are in the pages list
+                    if(pageList.includes(rule[0]) && pageList.includes(rule[1])) {
+                        //if it is make sure the rule is followed
+                        let firstPageIndex = pageList.indexOf(rule[0]);
+                        let secondPageIndex = pageList.indexOf(rule[1]);
+    
+                        if(firstPageIndex > secondPageIndex) {//if its not safe
+                            //swap the two numbers
+                            [pageList[firstPageIndex], pageList[secondPageIndex]] = 
+                                [pageList[secondPageIndex], pageList[firstPageIndex]];
+                            safe = false;
+                        }
+                    }
+            }
         }
 
         //if the list is safe
-        if(!safe) {
-            console.log(pageList);
-            
-            // let numList = pageList.split(/,/);
-            // //find the middle element
-            // let len = numList.length;
-            // //console.log(Math.floor(len/2));
-            // result += Number(numList[Math.floor(len/2)])
+        if(!firstSafe) {
+            //find the middle element
+            let len = pageList.length;
+            //console.log(Math.floor(len/2));
+            result += Number(pageList[Math.floor(len/2)])
         }
 
     });
